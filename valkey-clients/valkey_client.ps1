e9c79785cd91a88f1d6847bea9193e7751c373d1
pshankinclarke <38367844+pshankinclarke@users.noreply.github.com>
2024-06-07 13:48:28 -0700

    Update valkey_client.ps1
    Added auth

--- valkey-clients/valkey_client.ps1
+++ valkey-clients/valkey_client.ps1
@@ -5,7 +5,10 @@ $cacertPath = "/home/valkeyuser/valkey/tests/tls/acme-cert-authority.ca.public.p
 $serverHost = "server-alpha.network.local"
 $port = 6379
 
-$baseCommand = "wsl $valkeyClientPath --tls --cert $certPath --key $keyPath --cacert $cacertPath -h $serverHost -p $port"
+$username = [System.Environment]::GetEnvironmentVariable('VALKEY_USERNAME', 'Machine')
+$password = [System.Environment]::GetEnvironmentVariable('VALKEY_PASSWORD', 'Machine')
+
+$baseCommand = "wsl $valkeyClientPath --tls --cert $certPath --key $keyPath --cacert $cacertPath --user $username -h $serverHost -p $port -a $password"
 
 function Invoke-CommandSafely {
     param (
